f34461ecc6412a21eb2c72320da35ebce5e6e17c
edammame <139234613+edammame@users.noreply.github.com>
2023-07-18 02:38:14 +0700

    cheesepizza

---
+++ pizzaid.js
@@ -0,0 +1,7 @@
+function show(params) {
+  let image= document.getElementById('box');
+  image.src= "https://i.ebayimg.com/images/g/I88AAOSwmFBde3RN/s-l500.jpg";
+  document.getElementById("pizzabox")
+  .style.display= "none";
+
+}
